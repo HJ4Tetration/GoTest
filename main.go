0d18e1d4f77731205b744c6769d1a07101641b62
harojian <harojian@tetration.com>
2018-08-03 16:23:49 -0700

    functionClosure test finished

--- main.go
+++ main.go
@@ -17,6 +17,21 @@ func main() {
 	if i == 3 {
 		fmt.Println(2)
 	}
+	func main() {
+		m := make(map[string]int)
+	
+		m["Answer"] = 42
+		fmt.Println("The value:", m["Answer"])
+	
+		m["Answer"] = 48
+		fmt.Println("The value:", m["Answer"])
+	
+		delete(m, "Answer")
+		fmt.Println("The value:", m["Answer"])
+	
+		v, ok := m["Answer"]
+		fmt.Println("The value:", v, "Present?", ok)
+	}
 	//fmt.Println(foo1(1, 10))
 	//fmt.Println(foo2(1, 2))
 }
