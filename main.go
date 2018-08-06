9e657a273393c8ae15c5d9911cb9dfc9c5c7e780
harojian <harojian@tetration.com>
2018-08-06 16:15:33 -0700

    go tour finished

--- main.go
+++ main.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
+type I interface {
+	test()
+}
+
+type V struct {
+	x, y int
+}
+
+func (v *V) test() {
+	fmt.Println((*v).y)
+}
+
 func foo1(x int, y int) (int, int) {
 	return x + y, x
 }
@@ -12,26 +24,62 @@ func foo2(x int, y int) int {
 	return x + y
 }
 
-func main() {
-	var i int = 3
-	if i == 3 {
-		fmt.Println(2)
-	}
-	func main() {
-		m := make(map[string]int)
-	
-		m["Answer"] = 42
-		fmt.Println("The value:", m["Answer"])
-	
-		m["Answer"] = 48
-		fmt.Println("The value:", m["Answer"])
-	
-		delete(m, "Answer")
-		fmt.Println("The value:", m["Answer"])
-	
-		v, ok := m["Answer"]
-		fmt.Println("The value:", v, "Present?", ok)
-	}
-	//fmt.Println(foo1(1, 10))
-	//fmt.Println(foo2(1, 2))
+//var a = make([]int, 2, 2)
+
+func ts(t []int) {
+	t[0] = 100
 }
+
+var a = make([]int, 7, 8)
+
+func Test(slice []int) {
+	fmt.Println(len(slice))
+	slice = slice[:8]
+	fmt.Println(len(slice))
+	slice = append(slice[:7], 100)
+	fmt.Println(slice)
+}
+
+func main() {
+	for i := 0; i < 7; i++ {
+		a[i] = i
+	}
+
+	Test(a)
+	fmt.Println(a)
+}
+
+//func main() {
+//a := []byte{2, 2}
+//a := make([]byte, 2)
+//a[0] = 1
+//fmt.Printf("%v", a)
+//ts(a)
+//fmt.Printf("%v", a)
+/*st := V{1, 2}
+var interf I
+interf = &st
+interf.test()*/
+
+//var st *V
+//st.x = 1
+/*a := [8]byte{4, 4, 4, 4, 4, 4, 4, 4}
+for i, v := range a {
+	fmt.Println(i, v)
+}*/
+/*a := make([]byte, 0, 1)
+fmt.Println(cap(a))
+a[0] = 1
+a.append(1)
+a = a[:cap(a)]
+fmt.Println(cap(a))*/
+
+/*var i int = 3
+if i == 3 {
+	fmt.Println(2)
+}*/
+
+//}
+
+//fmt.Println(foo1(1, 10))
+//fmt.Println(foo2(1, 2))
